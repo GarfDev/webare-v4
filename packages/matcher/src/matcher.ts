cb7150bfbc0ca613dc9d7d67165d25fbc6f3786e
GarfDev <garfdev.13@gmail.com>
2022-01-30 21:08:07 +0700

    fix: add .vscode config to remove node_modules errors

--- packages/matcher/src/matcher.ts
+++ packages/matcher/src/matcher.ts
@@ -1,8 +1,12 @@
 import * as amqblib from "amqplib";
-import { CHANNEL } from "@webare/common"
+import { CHANNEL } from "@webare/common";
 import Config from "config";
 
 export default async function matcher() {
     const connection = await amqblib.connect(Config.AMQB_URL);
     const channel = await connection.createChannel();
+
+    channel.consume(CHANNEL.MATCHER, (msg) => {
+        if (!msg) return;
+    });
 }
